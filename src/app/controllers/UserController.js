571714db78685bebf14db36ffe6004696e20bf69
Hadesknight <fernandorato.0@hotmail.com>
2020-04-16 15:36:47 -0300

    Update User Ok

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -38,4 +38,50 @@ export default {
       return res.status(500).json({ error: 'Failed to create User ' });
     }
   },
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      city: Yup.string(),
+      uf: Yup.string().max(2),
+      birth_date: Yup.date(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    try {
+      if (!(await schema.isValid(req.body))) {
+        return res.status(400).json({ error: 'Validation Fails' });
+      }
+
+      const { oldPassword, email } = req.body;
+      const user = await User.findByPk(req.userId);
+
+      if (email !== user.email) {
+        const emailExist = await User.findOne({ where: { email } });
+
+        if (emailExist) {
+          return res.status(401).json({ error: 'email already exist' });
+        }
+      }
+
+      if (oldPassword && !(await user.checkPassword(oldPassword))) {
+        return res.status(401).json({ error: 'Invalid Password' });
+      }
+
+      const userUpdate = await user.update(req.body);
+
+      return res.json(userUpdate);
+    } catch (err) {
+      return res.status(500).json({ error: 'Error to update user' });
+    }
+  },
 };
